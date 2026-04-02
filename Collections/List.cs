f71191d2a544723d80a189ca625f573dbe6bc7ac
Landon <230772997+landon-codes@users.noreply.github.com>
2026-04-02 17:57:16 +0000

    Create the StorageList class

---
+++ Collections/List.cs
@@ -0,0 +1,38 @@
+namespace JsonStorage;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class StorageList<T> : Storage<T> where T : new()
+{
+    public new List<T> container = new List<T>();
+    private string file;
+
+    public StorageList(string filePath) : base(filePath)
+    {
+        file = filePath;
+    }
+
+    // Shorthand for StorageList.container.Clear()
+    public void Clear()
+    {
+        container.Clear();
+    }
+
+    // Load is ovverriden to provide support specificly for lists
+    public new void Load()
+    {
+        Check();
+        List<T>? loadObject = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(file));
+        if (loadObject != null)
+        {
+            container = loadObject;
+        }
+    }
+
+    // Shorthand for StorageList.container.Count
+    public int Count()
+    {
+        return container.Count;
+    }
+}
